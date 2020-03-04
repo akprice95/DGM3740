5467c7c739972ab280aae77dd6094ac09e814f86
Aaron Price <aaronprice@19142-APrice.local>
2020-03-03 20:13:24 -0700

    updates to fix nav

--- Template/js/main.js
+++ Template/js/main.js
@@ -2,12 +2,21 @@ function test() {
   const nav = document.getElementById("thisNav");
   nav.classList.toggle("hidden");
 }
-
 function resize() {
-  if (window.outerWidth > 560) {
-    var element = document.querySelector("#thisNav");
-    if (element.classList.contains("hidden")) {
-      element.classList.remove("hidden");
-    }
+  if (window.outerWidth <= 559) {
+    let element = document.querySelector("#thisNav");
+    element.classList.add("hidden");
   }
+  if (window.outerWidth > 559) {
+    const nav = document.getElementById("thisNav");
+    nav.classList.remove("hidden");
+  }
+}
+if (window.outerWidth <= 559) {
+  let element = document.querySelector("#thisNav");
+  element.classList.add("hidden");
+}
+if (window.outerWidth > 559) {
+  const nav = document.getElementById("thisNav");
+  nav.classList.remove("hidden");
 }
